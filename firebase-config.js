1d229eadb3b8573a49942810339ab27d9fd97fdf
omoboyejohn-dev <omoboyejohn@gmail.com>
2026-08-23 15:23:17 +0100

    Refactor Firebase configuration and exports

--- firebase-config.js
+++ firebase-config.js
@@ -1,49 +1,26 @@
-// Firebase SDK
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
 import {
-    getAuth
+  initializeApp
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+
+import {
+  getAuth
 } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
 
 import {
-    getFirestore
+  getFirestore
 } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
 
-
-// Your Firebase configuration
 const firebaseConfig = {
-
-    apiKey: "AIzaSyCu9P5eY1YCoeASRZDwOnKqWApq__2cWpY",
-
-    authDomain: "pizza-n-coke.firebaseapp.com",
-
-    projectId: "pizza-n-coke",
-
-    storageBucket: "pizza-n-coke.firebasestorage.app",
-
-    messagingSenderId: "172843629379",
-
-    appId: "1:172843629379:web:7f7539b409bae3c5c50df6",
-
-    measurementId: "G-QJX45CYPF4"
-
+  apiKey: "AIzaSyCu9P5eY1YCoeASRZDwOnKqWApq__2cWpY",
+  authDomain: "pizza-n-coke.firebaseapp.com",
+  projectId: "pizza-n-coke",
+  storageBucket: "pizza-n-coke.firebasestorage.app",
+  messagingSenderId: "172843629379",
+  appId: "1:172843629379:web:7f7539b409bae3c5c50df6",
+  measurementId: "G-QJX45CYPF4"
 };
 
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
-// Firebase Authentication
-const auth = getAuth(app);
-
-
-// Firestore Database
-const db = getFirestore(app);
-
-
-// Export for other pages
-export {
-    app,
-    auth,
-    db
-};
+export const auth = getAuth(app);
+export const db = getFirestore(app);
