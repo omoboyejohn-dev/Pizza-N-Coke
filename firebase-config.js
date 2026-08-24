14c33d8c821dd20b18dac907bc290a24d2d61c85
omoboyejohn-dev <omoboyejohn@gmail.com>
2026-08-24 23:17:27 +0100

    Refactor Firebase initialization and exports

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,5 @@
+// firebase-config.js
+
 import {
   initializeApp
 } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
@@ -10,17 +12,40 @@ import {
   getFirestore
 } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
 
+
 const firebaseConfig = {
+
   apiKey: "AIzaSyCu9P5eY1YCoeASRZDwOnKqWApq__2cWpY",
+
   authDomain: "pizza-n-coke.firebaseapp.com",
+
   projectId: "pizza-n-coke",
+
   storageBucket: "pizza-n-coke.firebasestorage.app",
+
   messagingSenderId: "172843629379",
+
   appId: "1:172843629379:web:7f7539b409bae3c5c50df6",
+
   measurementId: "G-QJX45CYPF4"
+
 };
 
-const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const app =
+  initializeApp(firebaseConfig);
+
+
+const auth =
+  getAuth(app);
+
+
+const db =
+  getFirestore(app);
+
+
+export {
+  app,
+  auth,
+  db
+};
